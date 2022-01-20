28048b8a14de3d38d920cb43ec3c091d9bd7c7b5
Boyquotes <webmaster@linux-live-cd.org>
2022-01-20 17:23:40 +0100

    Truffle config upgrade to 0.8.11 too

--- truffle-config.js
+++ truffle-config.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   compilers: {
     solc: {
-      version: "0.8.10",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.11",    // Fetch exact version from solc-bin (default: truffle's version)
       settings: {          // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
          enabled: false,
