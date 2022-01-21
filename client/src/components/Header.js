6b421ea35b0192f470f5a29d61482de67b5a58bf
Boyquotes <webmaster@linux-live-cd.org>
2022-01-21 13:30:49 +0100

    Homepage template leaf

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Navbar, Nav, Button } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+//import { LinkContainer } from 'react-router-bootstrap';
 import { init, connectWallet, checkConnection , switchChain , checkNetwork} from "../lib/web3";
 
 const Header = () => {
@@ -38,30 +38,7 @@ const Header = () => {
 
   return (
     <Navbar bg="light" expand="lg">
-      <LinkContainer to="/">
-        <Navbar.Brand>Home</Navbar.Brand>
-      </LinkContainer>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-
-        <Nav className="mr-auto">
-          <LinkContainer to="/mint">
-            <Nav.Link>Mint</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/nft">
-            <Nav.Link>NFT</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/team">
-            <Nav.Link>Team</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/roadmap">
-            <Nav.Link>Roadmap</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/about">
-            <Nav.Link>About</Nav.Link>
-          </LinkContainer>
-        </Nav>
-
+      <ul><li>Game</li></ul>
         {address && network != "avax" &&
           <Button onClick={() => switchNetwork()}>
              Switch Network
@@ -80,7 +57,6 @@ const Header = () => {
            </Button>
         }
 
-      </Navbar.Collapse>
     </Navbar>
   )
 }
