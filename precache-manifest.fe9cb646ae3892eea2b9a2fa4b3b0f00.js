8d619c8a20eaf142d29f136bc45efbaf07ff79bf
AramArDev <arakelyan.aram.dev@gmail.com>
2022-01-26 14:10:23 +0100

    Updates

---
+++ precache-manifest.fe9cb646ae3892eea2b9a2fa4b3b0f00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcab2f3548b209dfbe65d250d32d9e16",
+    "url": "/leaf_owner/index.html"
+  },
+  {
+    "revision": "e71b7bd079e4101905ee",
+    "url": "/leaf_owner/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "701226584262839b374e",
+    "url": "/leaf_owner/static/js/2.6107c4d0.chunk.js"
+  },
+  {
+    "revision": "e71b7bd079e4101905ee",
+    "url": "/leaf_owner/static/js/main.3db8e649.chunk.js"
+  },
+  {
+    "revision": "779c992f20fd50b6c5ea",
+    "url": "/leaf_owner/static/js/runtime-main.864d8069.js"
+  },
+  {
+    "revision": "6d95aa53b8f3e09815614e6be9411326",
+    "url": "/leaf_owner/static/media/Play_to_Plan_1.6d95aa53.gif"
+  },
+  {
+    "revision": "2d3105fe8306a384a030b8d17ebc3cbd",
+    "url": "/leaf_owner/static/media/forest-background-5-flou.2d3105fe.jpeg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/leaf_owner/precache-manifest.fe9cb646ae3892eea2b9a2fa4b3b0f00.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/leaf_owner/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
